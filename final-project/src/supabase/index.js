eb9325deef1d3993d2b337a7ce06709253c5d640
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-09-01 21:48:58 +0200

    basic configs 1

---
+++ final-project/src/supabase/index.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://dhikeiodkvfxtlsfkmvz.supabase.co';
+const supabaseAnonToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoaWtlaW9ka3ZmeHRsc2ZrbXZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE4ODAzNjYsImV4cCI6MTk3NzQ1NjM2Nn0.bCBnYf73tzUgLKalDQW8GPKEijJ61Dg-TXU35iZLg2E';
+
+export const supabase = createClient(supabaseUrl,supabaseAnonToken);
