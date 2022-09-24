2ac61122e5d727ab6ae01fa42f9890db09180060
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-09-24 15:26:10 +0200

    Smarter error handling & addedempty state

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,9 +1,14 @@
 <template>
+  <section class="emptystate" v-if="!tasks.length">
+    <img src="https://i.imgur.com/qN1iZfK.jpg" alt="task illustration" />
+    <h2>So far, nothing here</h2>
+    <p>Your tasks will appear here once you create them</p>
+  </section>
   <ul class="task-in-list">
     <li v-for="task in tasks" :key="task.id">
       <input
         class="checkbox"
-        @click="handleTaskStatus(task.is_complete, task.id);"
+        @click="handleTaskStatus(task.is_complete, task.id)"
         type="checkbox"
         v-model="task.is_complete"
         v-bind:id="task.is_complete"
@@ -12,16 +17,18 @@
         <input
           class="task-name"
           v-model="task.title"
-          :style="task.is_complete ? 'text-decoration: line-through': 'text-decoration:none'"
+          :style="
+            task.is_complete
+              ? 'text-decoration: line-through'
+              : 'text-decoration:none'
+          "
           @change="handleTaskName(task.title, task.id)"
         />
-        <span v-if="task.is_complete" id='done'> DONE!</span>
+        <span v-if="task.is_complete" id="done"> DONE!</span>
       </div>
       <button id="kill" @click="deleteTask(task.id)">Remove</button>
-
     </li>
   </ul>
-  
 </template>
 
 <script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,29 +4,16 @@
     <span> | Sign up</span>
   </header>
   <form>
-    <div v-if="errorMsg" class='error-message'>
-      <p>{{ errorMsg }}</p>
-    </div>
     <div>
       <label for="email"
         >Email:
-        <input
-          type="email"
-          required
-          id="email"
-          v-model="email"
-        />
+        <input type="email" required id="email" v-model="email" />
       </label>
     </div>
     <div>
       <label for="password"
         >Password:
-        <input
-          type="password"
-          required
-          id="password"
-          v-model="password"
-        />
+        <input type="password" required id="password" v-model="password" />
       </label>
     </div>
     <div>
@@ -41,6 +28,11 @@
       </label>
     </div>
     <button @click.prevent="handleSignUp">Sign up</button>
+    <Transition>
+      <div v-if="errorMsg" class="error-message">
+        <p>{{ errorMsg }}</p>
+      </div>
+    </Transition>
   </form>
 </template>
 
@@ -52,10 +44,10 @@ export default {
   name: "RegisterView",
   data() {
     return {
-      errorMsg: '',
-      email: '',
-      password: '',
-      confirmPass: '',
+      errorMsg: "",
+      email: "",
+      password: "",
+      confirmPass: "",
     };
   },
   computed: {
@@ -64,30 +56,41 @@ export default {
   methods: {
     ...mapActions(userStore, ["signUp"]),
     async handleSignUp() {
-      if(this.password === this.confirmPass) {
+      if (this.password === this.confirmPass) {
         try {
-      const userData = {
-        email: this.email,
-        password: this.password,
-      };
-      this.signUp(userData.email, userData.password);
-      }
-      catch (error) { 
-        this.errorMsg = error.message;
-       }
-      }
-      else this.errorMsg = 'Passwords do not match!';
+          const userData = {
+            email: this.email,
+            password: this.password,
+          };
+          this.signUp(userData.email, userData.password);
+        } catch (error) {
+          this.errorMsg = error.message;
+        }
+      } else this.errorMsg = "Passwords do not match!";
+      setTimeout(() => {
+        this.errorMsg = null;
+      }, 3000);
     },
   },
   watch: {
     user() {
       if (this.user) {
         this.$router.push({ path: "/" });
-      }
-      else {
-        this.$router.push({path: '/auth'})
+      } else {
+        this.$router.push({ path: "/auth" });
       }
     },
   },
 };
 </script>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
